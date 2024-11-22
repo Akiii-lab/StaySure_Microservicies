11a555be3744bed7c35405c158a9e569847a0ed1
Keiner Alvarado <kjgameplay0000@gmail.com>
2024-11-22 02:20:10 -0500

    feat: controllers

---
+++ src/main/java/StaySure/Controllers/LesseController.java
@@ -0,0 +1,49 @@
+package StaySure.Controllers;
+
+import java.util.HashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import StaySure.Repositories.DTO.LesseDTO;
+import StaySure.Services.LesseService;
+
+@RestController
+@RequestMapping("/api/lesse")
+public class LesseController {
+
+    @Autowired
+    LesseService service;
+
+    @GetMapping("/itself")
+    public ResponseEntity<HashMap<String, Object>> getitself() {
+        Authentication auth = (Authentication) SecurityContextHolder.getContext().getAuthentication();
+        HashMap<String, Object> response = new HashMap<>();
+        LesseDTO user = service.findLesseByEmail(auth.getName());
+        if (user == null) {
+            response.put("error", "User not found");
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        }
+        response.put("data", user);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<HashMap<String, Object>> getUserByID(@PathVariable("id") Long id) {
+        HashMap<String, Object> response = new HashMap<>();
+        LesseDTO user = service.findLesseById(id);
+        if (user == null) {
+            response.put("error", "User not found");
+            return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+        }
+        response.put("data", user);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
